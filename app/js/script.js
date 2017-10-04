d252820fca669278b22822541195ec82303e82d6
DRinsvind <adskiyprikol@gmail.com>
2017-10-04 17:49:51 +0300

    th

--- app/js/script.js
+++ app/js/script.js
@@ -18,29 +18,50 @@ $().ready(function() {
     setTimeout(function () {
         $('.solutions-list__logo').addClass('active');
     },500);
-    $('.solution-list__item').click(function(){
-        $('.solution-list__item').removeClass('active');
-        $(this).addClass('active');
-        var id = $(this).data('id');
-        $('.solution-content__item').removeClass('active');
-        $('.solution-content__item-'+id).addClass('active');
-    });
-    var urlSplit = window.location.hash;
-    $('.solution-list__item').each(function () {
-        console.log($(this).data('name'));
-        console.log($(this).data('name'));
-        if('#'+$(this).data('name')===urlSplit){
-            $(this).click();
+    var init = {
+
+        click:function(){
+            $('.modal-button').click(function(e){
+                e.preventDefault();
+                e.stopPropagation();
+                $('.white-filter').addClass('active');
+                $('.modal-block').addClass('active');
+            });
+            $('.close-but').click(function(e){
+                $('.white-filter').removeClass('active');
+                $('.modal-block').removeClass('active');
+            });
+            $('.solution-list__item').click(function(){
+                $('.solution-list__item').removeClass('active');
+                $(this).addClass('active');
+                var id = $(this).data('id');
+                $('.solution-content__item').removeClass('active');
+                $('.solution-content__item-'+id).addClass('active');
+            });
+        },
+        postRender:function(){
+            var urlSplit = window.location.hash;
+            $('.solution-list__item').each(function () {
+                console.log($(this).data('name'));
+                console.log($(this).data('name'));
+                if('#'+$(this).data('name')===urlSplit){
+                    $(this).click();
+                }
+            });
+        },
+        resizableFunctions:function(){
+            function mainBlock(){
+                $('section.main').height($(window).height()-$('header').height());
+            }
+            mainBlock();
         }
-    })
-    $('.modal-button').click(function(e){
-        e.preventDefault();
-        e.stopPropagation();
-        $('.white-filter').addClass('active');
-        $('.modal-block').addClass('active');
-    })
-    $('.close-but').click(function(e){
-        $('.white-filter').removeClass('active');
-        $('.modal-block').removeClass('active');
-    })
+    }
+    init.postRender();
+    init.click();
+    init.resizableFunctions();
+    $(window).resize(function(){
+        init.resizableFunctions();
+    });
+
+
 });
